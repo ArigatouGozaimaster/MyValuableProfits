73d9d4a393a1d87fce389a0533b62bb324361741
ArigatouGozaimaster <106367796+ArigatouGozaimaster@users.noreply.github.com>
2022-09-14 21:28:09 +1000

    sync

--- app.py
+++ app.py
@@ -18,11 +18,20 @@ from flask_bcrypt import Bcrypt
 
 
 
-# Create App
+# Initialise
 app = Flask(__name__)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+# Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Prepare Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = '$a1teDP@$$w0rd15Very1MP0Rtant'
 
